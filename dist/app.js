6e0d0ebad5fb19fd40462000284f33ba707bd2e1
Feridun <feridun@aerlabs.com>
2024-05-05 21:07:13 +0200

    created a class that renders the form in the index.html file

--- dist/app.js
+++ dist/app.js
@@ -1,3 +1,22 @@
 "use strict";
-console.log("Hellow rold");
+var ProjectInput = /** @class */ (function () {
+    function ProjectInput() {
+        this.templateElement = document.getElementById("project-input");
+        this.hostElement = document.getElementById("app");
+        // importNode also takes a second argument as a boolean.
+        // If true importNode will import a deep clone which imports with all levels of nesting
+        // If false it will import just the element itself
+        var importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild;
+        this.attach();
+    }
+    ProjectInput.prototype.attach = function () {
+        // insertAdjacentElement = to insert an HTML element
+        // 1st argument === string = where to insert
+        // 2st argument = another HTML element
+        this.hostElement.insertAdjacentElement("afterbegin", this.element);
+    };
+    return ProjectInput;
+}());
+var prjInput = new ProjectInput();
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,32 @@
-console.log("Hellow rold");
+class ProjectInput {
+  templateElement: HTMLTemplateElement;
+  // we could also just write HTMLElement but since we know it is going to be a div we can specify it.
+  hostElement: HTMLDivElement;
+  element: HTMLFormElement;
+
+  constructor() {
+    this.templateElement = document.getElementById(
+      "project-input"
+    )! as HTMLTemplateElement;
+    this.hostElement = document.getElementById("app")! as HTMLDivElement;
+
+    // importNode also takes a second argument as a boolean.
+    // If true importNode will import a deep clone which imports with all levels of nesting
+    // If false it will import just the element itself
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+    this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.attach();
+  }
+
+  private attach() {
+    // insertAdjacentElement = to insert an HTML element
+    // 1st argument === string = where to insert
+    // 2st argument = another HTML element
+    this.hostElement.insertAdjacentElement("afterbegin", this.element);
+  }
+}
+
+const prjInput = new ProjectInput();
